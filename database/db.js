960a45fe1f047054d78d89736d89f09ad5ecc074
Fabel Florian De la Cruz <FabelFlorianDelaCruz@hotmail.com>
2025-07-31 23:29:14 -0400

    Create y correccion de errores

--- database/db.js
+++ database/db.js
@@ -1,8 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/HelpDesk', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Conectado a MongoDB local'))
-.catch((err) => console.error('Error al conectar a MongoDB:', err));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/HelpDesk", {})
+  .then(() => console.log("Conectado a MongoDB local"))
+  .catch((err) => console.error("Error al conectar a MongoDB:", err));
